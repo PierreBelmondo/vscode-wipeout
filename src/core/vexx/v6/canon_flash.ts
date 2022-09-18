63d4d8fa6c6799b90008aecb37b00aecf388bcdd
Pierre Belmondo <p.belmondo@proton.me>
2022-09-18 11:55:17 +0200

    adds missing vexx v6 node types

---
+++ src/core/vexx/v6/canon_flash.ts
@@ -0,0 +1,8 @@
+import { VexxNode } from "../node";
+import { Vexx6NodeType } from "./type";
+
+export class VexxNodeCanonFlash extends VexxNode {
+  constructor() {
+    super(Vexx6NodeType.CANON_FLASH);
+  }
+}
---
+++ src/core/vexx/v6/sound_cone.ts
@@ -0,0 +1,8 @@
+import { VexxNode } from "../node";
+import { Vexx6NodeType } from "./type";
+
+export class VexxNodeSoundCone extends VexxNode {
+  constructor() {
+    super(Vexx6NodeType.SOUND_CONE);
+  }
+}
