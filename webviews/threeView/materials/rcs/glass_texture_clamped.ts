42dde26a083fef71524dfff97f5e81d416094adb
Pierre Belmondo <p.belmondo@proton.me>
2023-05-01 11:54:23 +0200

    rcsmaterial: fix min/max textures on glass_texture_clamped

--- webviews/threeView/materials/rcs/glass_texture_clamped.ts
+++ webviews/threeView/materials/rcs/glass_texture_clamped.ts
@@ -10,8 +10,8 @@ import { MaterialFactory } from "./_abstract";
 */
 export const glass_texture_clamped: MaterialFactory = {
   name: "glass_texture_clamped.rcsmaterial",
-  minTextures: 0,
-  maxTextures: 0,
+  minTextures: 1,
+  maxTextures: 1,
   make: (textures: THREE.Texture[]) => {
     return new THREE.MeshPhongMaterial({
       side: THREE.DoubleSide,
