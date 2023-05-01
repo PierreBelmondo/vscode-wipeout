6855d51c3ba28c26a753b53cbae274d2fe6dced8
Pierre Belmondo <p.belmondo@proton.me>
2023-05-01 11:54:56 +0200

    rcsmaterial: fix min/max textures on tracktexture_with_normal

--- webviews/threeView/materials/rcs/tracktexture_with_normal.ts
+++ webviews/threeView/materials/rcs/tracktexture_with_normal.ts
@@ -14,14 +14,15 @@ import { MaterialFactory } from "./_abstract";
 */
 export const tracktexture_with_normal: MaterialFactory = {
   name: "tracktexture_with_normal.rcsmaterial",
-  textures: 2,
+  minTextures: 2,
+  maxTextures: 2,
   make: (textures: THREE.Texture[]) => {
     return new THREE.MeshPhongMaterial({
       side: THREE.DoubleSide,
       color: 0xffffff,
       specular: 0xffffff,
-      //map: textures[0],
-      //specularMap: textures[0],
+      map: textures[0],
+      specularMap: textures[0],
       normalMap: textures[1],
       vertexColors: true,
     });
